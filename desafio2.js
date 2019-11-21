543e2443e004d23c4721661039c41a8c4503ee9c
Guilherme Alves <guilherme.alves@ad.wetoksoft.com.br>
2019-11-21 18:03:04 -0300

    desafio 1 e 2

---
+++ desafio2.js
@@ -0,0 +1,110 @@
+// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+function soma(x, y){
+	return x+y; 
+}
+
+// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, 
+//e somando `5` ao resultado retornado da função.
+var somaPlus = soma(5,20) + 5;
+
+
+// Qual o valor atualizado dessa variável?
+30
+
+// Declare uma nova variável, sem valor.
+var semvalor;
+
+/*
+Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+    O valor da variável agora é VALOR.
+Onde VALOR é o novo valor da variável.
+*/
+function adicionaValor(){
+	semvalor = 10;
+	return 'O valor da variável agora é ' + semvalor;
+}
+
+// Invoque a função criada acima.
+adicionaValor()
+
+// Qual o retorno da função? (Use comentários de bloco).
+
+//O valor da Variável agora é 10
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+*/
+function tres(a,b,c){
+	if (a == null || b == null || c == null) {
+		return ' Preencha todos os valores corretamente!'; 
+	}
+	else{
+		return (a * b) + c
+	}
+}
+
+// Invoque a função criada acima, passando só dois números como argumento.
+
+tres(2,2)
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+
+//Preencha todos os valores corretamente!
+
+// Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+
+tres(2,2,10)
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+	
+//14
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos.
+2. Se somente um argumento for passado, retorne o valor do argumento.
+3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+5. Se nenhum argumento for passado, retorne o valor booleano `false`.
+6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
+*/
+
+
+function argumento(x,y,z){
+	if (x != null && y == null && z == null) {
+		return 'x = ' + x; 
+	}
+	else if (y != null && x == null && z == null){
+		return 'y = ' + y;
+	}
+	else if (z != null && x == null && y == null){
+		return 'z = ' +  z;
+	}
+	else if (x != null && y != null && z == null) {
+		return 'x + y = ' + (x + y);
+	} 
+	else if (x != null && z != null && y == null) {
+		return 'x + z = ' + (x + z);
+	}	
+	else if (y != null && z != null && x == null) {
+		return 'y + x = ' + (y + z); 
+	}
+	else if (x != null && y != null && z != null){
+		return 'x + y + z = ' + ((x+y)/z);
+	}
+	else if (x == undefined && y == undefined && z == undefined){
+		return false;
+	}
+	else{
+		return null;
+	}	
+}			 
+
+
+
+// Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+?
